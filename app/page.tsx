2b84a05380c63e03e3004ca36497cc28fe2d3635
Bryan <bryanmisael0819@gmail.com>
2025-12-09 23:47:18 -0400

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import {Button} from "@/components/ui/button";
+
+function Inicio() {
+    return (
+        <div className="flex justify-center items-center h-screen"><Button>click me</Button></div>
+    )
+}
+
+export default Inicio
